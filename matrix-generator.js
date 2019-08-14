4d18198417cb794d82b92297c3de519510777927
Keith <kayzee@live.com>
2019-08-14 11:55:03 -1000

    completed

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,21 @@
+"use stric";
+
 function matrixGenerator(rows, columns) {
-  // do work here
+  const array = [];
+  for (let i = 0; i < rows; i++) {
+    let row = [];
+    for (let j = 0; j < columns; j++) {
+      row.push(Math.floor(Math.random() * 100));
+    }
+    row.sort((a, b) => {
+      return a - b;
+    });
+    array.push(row);
+  }
+  array.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  return array;
 }
 
 module.exports = matrixGenerator;
